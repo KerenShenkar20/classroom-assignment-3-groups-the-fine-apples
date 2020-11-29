44fc7c1717b2f0aa6916a18d1ecf8268bf717aa3
or kilim <48524233+orkilim@users.noreply.github.com>
2020-11-29 14:44:21 +0200

    Or's route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.json({response: "index route"}));
-
+app.post('/addLanguage',(req,res)=>{res.status(200).send("language added, let's go best team")})
 /*  Every team member has to add a route to this file, like the one above
     For example: 
     Student A writes the route for GET /playlist
